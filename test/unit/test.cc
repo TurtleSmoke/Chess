0fa803a2dd46b9808055e145cef50f9fdbe93f2f
TurtleSmoke <46165291+TurtleSmoke@users.noreply.github.com>
2021-09-26 13:51:02 +0200

    test: add basic test for coverage

---
+++ test/unit/test.cc
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+#include "test_tmp.hh"
+
+TEST(foo, bar) {
+    ASSERT_EQ(f1(1), 0);
+}
+
+TEST(foo, bar2) {
+    ASSERT_EQ(f1(-1), 1);
+}
+
+TEST(foo, bar3) {
+    ASSERT_EQ(f1(0), 1);
+}
