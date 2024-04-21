9d0ed718c15e0ac3837b1f27e5deeb70c6fd4669
TurtleSmoke <46165291+TuWG@users.noreply.github.com>
2024-04-21 12:38:56 +0200

    refactor(clang): update clang-tidy and add header folder path

--- utils/run_clang_tidy.sh
+++ utils/run_clang_tidy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Run clang-tidy on all files in the project
-# shellcheck disable=SC2046
-clang-tidy -extra-arg=-std=c++17 $(find src -name "*.cc" -o -name "*.hh") --
+clang-tidy -extra-arg=-std=c++17 $(find src -name "*.cc" -o -name "*.hh") -- -Isrc/engine
