f5cff513d340f0d68f5230d8d10b501713b191dd
TurtleSmoke <46165291+TuWG@users.noreply.github.com>
2024-05-28 00:03:53 +0200

    feat(moves): add pawn promotion

--- src/engine/moves/pawn.hh
+++ src/engine/moves/pawn.hh
@@ -60,6 +60,50 @@ Move *generate_pawn_attacks(const Board &board, Move *moves) {
     return moves;
 }
 
+template <Direction Direction>
+Move *make_promotion(const Square to, Move *moves) {
+    *moves++ = Move(to - Direction, to, KNIGHT);
+    *moves++ = Move(to - Direction, to, BISHOP);
+    *moves++ = Move(to - Direction, to, ROOK);
+    *moves++ = Move(to - Direction, to, QUEEN);
+
+    return moves;
+}
+
+template <Color TURN>
+Move *generate_pawn_promotions(const Board &board, Move *moves) {
+    constexpr Bitboard promotion_rank = TURN == WHITE ? RANK_7_BB : RANK_2_BB;
+    constexpr Direction push_direction = TURN == WHITE ? NORTH : SOUTH;
+    constexpr Direction left_attack = TURN == WHITE ? NORTH_WEST : SOUTH_EAST;
+    constexpr Direction right_attack = TURN == WHITE ? NORTH_EAST : SOUTH_WEST;
+
+    const Bitboard emptySquares = ~board.get_pieces();
+    const Bitboard their_pieces = board.colors[~TURN];
+
+    Bitboard pawns = board.pieces[PAWN] & board.colors[TURN];
+    Bitboard promoting_pawns = pawns & promotion_rank;
+
+    if (promoting_pawns) {
+        Bitboard single_push = (shift<push_direction>(promoting_pawns) & emptySquares);
+        Bitboard left_attacks = (shift<left_attack>(promoting_pawns) & their_pieces);
+        Bitboard right_attacks = (shift<right_attack>(promoting_pawns) & their_pieces);
+
+        while (single_push) {
+            moves = make_promotion<push_direction>(pop_lsb(single_push), moves);
+        }
+
+        while (left_attacks) {
+            moves = make_promotion<left_attack>(pop_lsb(left_attacks), moves);
+        }
+
+        while (right_attacks) {
+            moves = make_promotion<right_attack>(pop_lsb(right_attacks), moves);
+        }
+    }
+
+    return moves;
+}
+
 }  // namespace chess
 
 #endif  // CHESS_PAWN_HH
--- tests/unit/moves/test_pawn.cc
+++ tests/unit/moves/test_pawn.cc
@@ -24,6 +24,11 @@ namespace chess {
         ASSERT_MOVES(fen, expected, turn, generate_pawn_attacks); \
     } while (0)
 
+#define ASSERT_PROMOTIONS(fen, expected, turn)                       \
+    do {                                                             \
+        ASSERT_MOVES(fen, expected, turn, generate_pawn_promotions); \
+    } while (0)
+
 // Pawn pushes
 TEST(PAWN_MOVES, white_pawn_starting) { ASSERT_PUSHES("8/8/8/8/8/8/PPPPPPPP/8 w HAha - 0 1", 16, WHITE); }
 
@@ -53,4 +58,21 @@ TEST(PAWN_MOVES, black_pawn_double_attacks) { ASSERT_ATTACKS("8/4p3/3P1P2/8/8/8/
 TEST(PAWN_MOVES, black_pawn_many_attacks) { ASSERT_ATTACKS("8/p1p1p1p1/1P1P1P1P/8/8/8/8/8 b HAha - 0 1", 7, BLACK); }
 
 TEST(PAWN_MOVES, black_pawn_no_attacks) { ASSERT_ATTACKS("8/4P3/3p4/8/8/8/8/8 b HAha - 0 1", 0, BLACK); }
+
+// Pawn promotions
+TEST(PAWN_MOVES, white_pawn_promotion) { ASSERT_PROMOTIONS("8/3P4/8/8/8/8/8/8 w HAha - 0 1", 4, WHITE); }
+
+TEST(PAWN_MOVES, white_pawn_attacks_only) { ASSERT_PROMOTIONS("2ppp3/3P4/8/8/8/8/8/8 w HAha - 0 1", 8, WHITE); }
+
+TEST(PAWN_MOVES, white_pawn_all) { ASSERT_PROMOTIONS("2p1p3/3P4/8/8/8/8/8/8 w HAha - 0 1", 12, WHITE); }
+
+TEST(PAWN_MOVES, white_pawn_hard) { ASSERT_PROMOTIONS("2ppp3/3PPPP1/8/8/8/8/8/8 w HAha - 0 1", 24, WHITE); }
+
+TEST(PAWN_MOVES, black_pawn_promotion) { ASSERT_PROMOTIONS("8/8/8/8/8/8/3p4/8 b HAha - 0 1", 4, BLACK); }
+
+TEST(PAWN_MOVES, black_pabn_attacks_only) { ASSERT_PROMOTIONS("8/8/8/8/8/8/3p4/2PPP3 b HAha - 0 1", 8, BLACK); }
+
+TEST(PAWN_MOVES, black_pabn_all) { ASSERT_PROMOTIONS("8/8/8/8/8/8/3p4/2P1P3 b HAha - 0 1", 12, BLACK); }
+
+TEST(PAWN_MOVES, black_pabn_hard) { ASSERT_PROMOTIONS("8/8/8/8/8/8/3pppp1/2PPP3 b HAha - 0 1", 24, BLACK); }
 }  // namespace chess
